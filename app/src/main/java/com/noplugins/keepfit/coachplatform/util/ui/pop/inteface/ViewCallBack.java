65e50f3c720a9779655011d13fb1b466a26ab823
joyce <yangyanzhen@noplugins.onaliyun.com>
2019-11-29 13:20:23 +0800

    教练端pop集成

---
+++ app/src/main/java/com/noplugins/keepfit/coachplatform/util/ui/pop/inteface/ViewCallBack.java
@@ -0,0 +1,9 @@
+package com.noplugins.keepfit.coachplatform.util.ui.pop.inteface;
+
+import android.view.View;
+import com.lxj.xpopup.core.BasePopupView;
+
+public interface ViewCallBack {
+    void onReturnView(View view, BasePopupView popup);
+
+}
