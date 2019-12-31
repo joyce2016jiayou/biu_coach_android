81ef944eb681b7d34216268378306de12a2f2cdb
joyce <yangyanzhen@noplugins.onaliyun.com>
2019-12-31 17:20:47 +0800

    教练端

---
+++ WheelView/src/main/java/cn/qqtheme/framework/wheelview/entity/DateEntity.java
@@ -0,0 +1,47 @@
+package cn.qqtheme.framework.wheelview.entity;
+
+import java.util.Calendar;
+
+/**
+ * 日期数据实体
+ *
+ * @author <a href="mailto:1032694760@qq.com">liyujiang</a>
+ * @date 2019/6/17 15:29
+ */
+@SuppressWarnings({"unused"})
+public class DateEntity extends DateTimeEntity {
+
+    public static DateEntity today() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return new DateEntity(year, month, day);
+    }
+    public static DateEntity to3day() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 3);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return new DateEntity(year, month, day);
+    }
+
+    public static DateEntity to30day() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 30);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return new DateEntity(year, month, day);
+    }
+
+    public DateEntity(int year, int month, int day) {
+        super(year, month, day, 0, 0, 0);
+    }
+
+    public DateEntity(int month, int day) {
+        this(Calendar.getInstance().get(Calendar.YEAR), month, day);
+    }
+
+}
